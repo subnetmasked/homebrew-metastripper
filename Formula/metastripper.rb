0f8c2f30169f394b464fc0b1660c088e2950177d
VictorBjorke <144603139+subnetmasked@users.noreply.github.com>
2025-03-29 22:23:59 +0100

    Update formula to v0.1.5 with metadata reporting feature

--- Formula/metastripper.rb
+++ Formula/metastripper.rb
@@ -1,8 +1,8 @@
 class Metastripper < Formula
   desc "A cross-platform command-line tool for removing privacy-sensitive metadata from files"
   homepage "https://github.com/subnetmasked/MetaStripper"
-  url "https://github.com/subnetmasked/MetaStripper/archive/v0.1.4.tar.gz"
-  sha256 "110e76e7c36e7b694719c83a0f24c9a00d60bc561e5982d76256c962b513e02f"
+  url "https://github.com/subnetmasked/MetaStripper/archive/v0.1.5.tar.gz"
+  sha256 "9c371d88665bd0ded40e9dec2d5cd964ad545c18b02ac73b150dc944f97f80c3"
 
   depends_on "rust" => :build
   depends_on "ffmpeg" => :recommended
