d378593e26f0416afa7ecc8087aa0b92d0cf900e
VictorBjorke <144603139+subnetmasked@users.noreply.github.com>
2025-03-29 17:22:02 +0100

    Update formula to version 0.1.3

--- Formula/metastripper.rb
+++ Formula/metastripper.rb
@@ -1,8 +1,8 @@
 class Metastripper < Formula
   desc "A cross-platform command-line tool for removing privacy-sensitive metadata from files"
   homepage "https://github.com/subnetmasked/MetaStripper"
-  url "https://github.com/subnetmasked/MetaStripper/archive/v0.1.2.tar.gz"
-  sha256 "99dd065ee48b3874c6cd48286898ff932fb5461e5a2b95e1e40e6535a0e4bfd5"
+  url "https://github.com/subnetmasked/MetaStripper/archive/v0.1.3.tar.gz"
+  sha256 "143cbf3b9415a229bb62c445705c45a87c2af15addb5d456fc9036c7ca5dfb50"
 
   depends_on "rust" => :build
   depends_on "ffmpeg" => :recommended
